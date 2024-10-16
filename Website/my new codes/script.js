e5d1862afcdee6f9f98a2c459af5a647d65e4777
jehu45 <jehujahswill@gmail.com>
2024-10-16 14:06:11 +0100

    Add files via upload

---
+++ Website/my new codes/script.js
@@ -0,0 +1,25 @@
+document.getElementById("demo").innerHTML ="<h1>Hello world</h1>";
+document.querySelector(".front").innerHTML = "MY NAME IS JAHSWILL";
+
+document.getElementById("demo").style.color="red"; 
+
+document.getElementById("hide").style.display="none";
+
+//variables are containers for storing data
+//variables are declared in 4 ways automatically, using var, using let, using const
+
+var boy = "Jahswill";
+var age  = 18;
+var gender = "male"
+console.log(boy + age)
+alert(boy + age)
+
+//let a= 15;
+//let b= 30;
+//let c = "boy";
+//console.log(a+b)
+//alert(a+b)
+
+//const boy = "Chinedu";
+//console.log(boy)
+
